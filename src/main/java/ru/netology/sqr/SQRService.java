3288134ab64e93d9b724e3fa3a9e78c4ea982048
Маргарита Мусатова <musatovamargarita@gmail.com>
2022-07-13 00:04:09 +0300

    First saved

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,16 @@
+package ru.netology.sqr;
+
+public class SQRService {
+
+    int s = 0;
+
+    public int calcAmountSgr(int min, int max) {
+        for (int i = 10; i <= 99; i++) {
+            if (i * i >= min && i * i <= max) {
+                s = s + 1;
+            }
+        }
+        return s;
+
+    }
+}
---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,23 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+//import ru.netology.sqr.SQRService;
+
+public class SQRServiceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/values.csv")
+
+    public void shoulCalcExact(int expected, int min, int max) {
+        SQRService service = new SQRService();
+
+        //int expected = 3;
+        int actual = service.calcAmountSgr(min, max);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
